044b0ec889a22fd6f71dfc389097f4cee831e610
Gregorio <gregoriocarranza@hotmail.com>
2022-10-19 02:04:23 -0300

    Compotamiento del menu

--- Assets/Script/MenuScript/ComportamientoDeMenu.cs
+++ Assets/Script/MenuScript/ComportamientoDeMenu.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class ComportamientoDeMenu : MonoBehaviour
 {
     private static ComportamientoDeMenu instance;
+    [SerializeField] private GameObject[] Menues;
+    private int i = 0;
     public bool IniciarNivel;
     public int IndiceNivel;
 
@@ -17,9 +19,26 @@ public class ComportamientoDeMenu : MonoBehaviour
             CambiarNivel(IndiceNivel);
         }
     }
-
+    public void exit()
+    {
+        Application.Quit();
+    }
     public void CambiarNivel(int Indice)
     {
         SceneManager.LoadScene(Indice);
     }
+    public void PasarPantallaMenu()
+    {
+        i += 1;
+        Menues[i - 1].SetActive(false);
+        Menues[i].SetActive(true);
+    }
+    public void VolverPantallaMenu()
+    {
+        i -= 1;
+        Menues[i + 1].SetActive(false);
+        Menues[i].SetActive(true);
+    }
+
+
 }
