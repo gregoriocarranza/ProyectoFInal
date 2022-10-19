fb3adae60e9320627754c2a5eaf432faf06da267
Gregorio <gregoriocarranza@hotmail.com>
2022-10-18 22:06:44 -0300

    Nivel

--- Assets/Script/Enemy/EnemyMunnitionBehaviour/MisilAssetsScriots/BoomScript.cs
+++ Assets/Script/Enemy/EnemyMunnitionBehaviour/MisilAssetsScriots/BoomScript.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BoomScript : MonoBehaviour
 {
     AudioSource FinAudio;
+
     void Start()
     {
         FinAudio = GetComponent<AudioSource>();
 
         FinAudio.PlayOneShot(FinAudio.clip, 0.7F);
         Destroy(gameObject, 4f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerData>().damage(5);
+        }
     }
 
 
