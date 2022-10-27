14cf8db6d32311c0ec72415d9448fecd8cce0318
franlevin <54724159+franlevin@users.noreply.github.com>
2022-10-27 01:09:25 -0300

    Agrego sonido de healing - Fixeo movimiento en Y de player y trigger de mina

--- Assets/Script/Items/Botiquin.cs
+++ Assets/Script/Items/Botiquin.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Botiquin : MonoBehaviour
 {
     public int BotiquinHealed = 1;
+    public static Action playerHealed;
+
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
+            playerHealed?.Invoke();
             other.gameObject.GetComponent<PlayerData>().heal(BotiquinHealed);
             Destroy(gameObject);
         }
--- Assets/Script/Player/PlayerSound.cs
+++ Assets/Script/Player/PlayerSound.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PlayerSound : MonoBehaviour
 {
     AudioSource stepSound;
+    AudioSource healSound;
 
     // Start is called before the first frame update
     void Start()
     {
+        Botiquin.playerHealed += OnPlayerHeal;
+        
         AudioSource[] audios = GetComponents<AudioSource>();
         stepSound = audios[0];
+        healSound = audios[1];
+        //damageSound = audios[2];
     }
 
     // Update is called once per frame
@@ -25,5 +30,15 @@ public class PlayerSound : MonoBehaviour
         {
             stepSound.enabled = false;
         }
+    }
+
+    private void OnDisable()
+    {
+        //UIManager.Pause -= OnPause;
+    }
+
+    private void OnPlayerHeal()
+    {
+        healSound.Play();
     }
 }
